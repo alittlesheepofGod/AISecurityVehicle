b1bc89140829b3a0a4e15a5d7d953a5b0c98e562
chau <huynhminhchau.k14@gmail.com>
2022-03-21 23:54:02 +0900

    Improved Model 2 : Weight Decay

--- cnnCIFAR10.py
+++ cnnCIFAR10.py
@@ -12,6 +12,7 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.regularizers import l2
  
 # load train and test dataset
 def load_dataset():
@@ -36,15 +37,21 @@ def prep_pixels(train, test):
 # define cnn model
 def define_model():
 	model = Sequential()
-	# model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
-	# model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	# model.add(MaxPooling2D((2, 2)))
-	# model.add(Flatten())
-	# model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-	# model.add(Dense(10, activation='softmax'))
-	# # compile model
-	# opt = SGD(lr=0.001, momentum=0.9)
-	# model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', kernel_regularizer=l2(0.001), input_shape=(32, 32, 3)))
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', kernel_regularizer=l2(0.001)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', kernel_regularizer=l2(0.001)))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', kernel_regularizer=l2(0.001)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', kernel_regularizer=l2(0.001)))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', kernel_regularizer=l2(0.001)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform', kernel_regularizer=l2(0.001)))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 	return model
  
 # plot diagnostic learning curves
