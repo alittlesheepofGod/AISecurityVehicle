6d8be4db722fef2fe69b23a8ac4a55929e44dde1
chau <huynhminhchau.k14@gmail.com>
2022-03-22 15:42:56 +0900

    Further Improvements : Variation of Dropout Regularization

--- cnnCIFAR10.py
+++ cnnCIFAR10.py
@@ -12,7 +12,8 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.optimizers import SGD
- 
+from tensorflow.keras.layers import Dropout
+
 # load train and test dataset
 def load_dataset():
 	# load dataset
@@ -36,15 +37,25 @@ def prep_pixels(train, test):
 # define cnn model
 def define_model():
 	model = Sequential()
-	# model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
-	# model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
-	# model.add(MaxPooling2D((2, 2)))
-	# model.add(Flatten())
-	# model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
-	# model.add(Dense(10, activation='softmax'))
-	# # compile model
-	# opt = SGD(lr=0.001, momentum=0.9)
-	# model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.2))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.3))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.4))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dropout(0.5))
+	model.add(Dense(10, activation='softmax'))
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 	return model
  
 # plot diagnostic learning curves
